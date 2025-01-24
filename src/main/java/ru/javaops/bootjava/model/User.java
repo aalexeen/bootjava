492be2ba51d77554f695723c2ba7b7f1f8ab4c2f
aalexeen <alexan.sergeev@gmail.com>
2025-01-24 14:46:46 -0500

    1_01_user_with_lombok. Lombok annotation @Slf4j added.

--- src/main/java/ru/javaops/bootjava/model/User.java
+++ src/main/java/ru/javaops/bootjava/model/User.java
@@ -3,12 +3,14 @@ package ru.javaops.bootjava.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Slf4j
 public class User {
 
     private String email;
