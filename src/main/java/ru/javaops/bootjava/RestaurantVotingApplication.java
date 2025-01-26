b081a5a8571cf0450ddbb2476da237be1c0bd3fb
Aleksandr Alekseenko <alexan.sergeev@gmail.com>
2025-01-26 11:25:14 -0500

    2_02_h2_init

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -13,18 +13,18 @@ import java.util.Set;
 
 @SpringBootApplication
 @AllArgsConstructor
-    public class RestaurantVotingApplication implements ApplicationRunner {
-        private final UserRepository userRepository;
+public class RestaurantVotingApplication implements ApplicationRunner {
+    private final UserRepository userRepository;
 
         public static void main(String[] args) {
             SpringApplication.run(RestaurantVotingApplication.class, args);
         }
 
-        @Override
-        public void run(ApplicationArguments args) {
-            userRepository.save(new User("user@gmail.com", "User_First", "User_Last", "password", Set.of(Role.ROLE_USER)));
-            userRepository.save(new User("admin@javaops.ru", "Admin_First", "Admin_Last", "admin", Set.of(Role.ROLE_USER, Role.ROLE_ADMIN)));
-            System.out.println(userRepository.findAll());
-        }
-    }
 
+    @Override
+    public void run(ApplicationArguments args) {
+        //userRepository.save(new User("user@gmail.com", "User_First", "User_Last", "password", Set.of(Role.ROLE_USER)));
+        //userRepository.save(new User("admin@javaops.ru", "Admin_First", "Admin_Last", "admin", Set.of(Role.ROLE_USER, Role.ROLE_ADMIN)));
+        System.out.println(userRepository.findAll());
+    }
+}
