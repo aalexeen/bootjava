ba442bc774d019a950423018b5874281a843504e
aalexeen <alexan.sergeev@gmail.com>
2025-01-27 11:57:18 -0500

    7_03_remove_data_rest_refactoring

--- src/main/java/ru/javaops/bootjava/repository/UserRepository.java
+++ src/main/java/ru/javaops/bootjava/repository/UserRepository.java
@@ -1,15 +1,7 @@
 package ru.javaops.bootjava.repository;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javaops.bootjava.model.User;
 
@@ -17,14 +9,12 @@ import java.util.Optional;
 
 @Transactional(readOnly = true)
 @Tag(name = "User Controller")
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends BaseRepository<User> {
 
-    @RestResource(rel = "by-email", path = "by-email")
     @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
-    @Cacheable("users")
     Optional<User> findByEmailIgnoreCase(String email);
 
-    @RestResource(rel = "by-lastname", path = "by-lastname")
+    /*@RestResource(rel = "by-lastname", path = "by-lastname")
     Page<User> findByLastNameContainingIgnoreCase(String lastName, Pageable page);
 
     Page<User> findAll(Pageable page);
@@ -45,5 +35,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Modifying
     @Transactional
     @CacheEvict(value = "users", allEntries = true)
-    void deleteById(Integer integer);
+    void deleteById(Integer integer);*/
 }
