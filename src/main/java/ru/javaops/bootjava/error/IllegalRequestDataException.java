37f029916595f8b8aefdbba0a1bb4241b8064026
aalexeen <alexan.sergeev@gmail.com>
2025-01-27 15:38:46 -0500

    8_01_migrate_spring_boot_3_1_2

--- src/main/java/ru/javaops/bootjava/error/IllegalRequestDataException.java
+++ src/main/java/ru/javaops/bootjava/error/IllegalRequestDataException.java
@@ -1,12 +1,15 @@
 package ru.javaops.bootjava.error;
 
-import org.springframework.boot.web.error.ErrorAttributeOptions;
-import org.springframework.http.HttpStatus;
+//import org.springframework.boot.web.error.ErrorAttributeOptions;
+//import org.springframework.http.HttpStatus;
 
-import static org.springframework.boot.web.error.ErrorAttributeOptions.Include.*;
+//import static org.springframework.boot.web.error.ErrorAttributeOptions.Include.*;
+
+//import static org.springframework.boot.web.error.ErrorAttributeOptions.Include.MESSAGE;
 
 public class IllegalRequestDataException extends AppException {
     public IllegalRequestDataException(String msg) {
-        super(HttpStatus.UNPROCESSABLE_ENTITY, msg, ErrorAttributeOptions.of(MESSAGE, STACK_TRACE, EXCEPTION));
+        //super(HttpStatus.UNPROCESSABLE_ENTITY, msg, ErrorAttributeOptions.of(MESSAGE, STACK_TRACE, EXCEPTION));
+        super(msg);
     }
 }
